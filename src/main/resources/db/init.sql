1f79418ae3ba7ddc74504fc4bba9f5a1699eb570
iskorostinskiinikita <nikita11042000@gmail.com>
2025-03-27 22:48:56 +0300

    ci/cd: fixed init.sql script

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -1,24 +1,24 @@
 CREATE TABLE post (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    picture TEXT,
-    content TEXT NOT NULL,
+    image_path TEXT,
+    text TEXT NOT NULL,
     likes_count INT DEFAULT 0
 );
 
 CREATE TABLE tag (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE post_tag (
-    post_id INT REFERENCES post(id) ON DELETE CASCADE,
-    tag_id INT REFERENCES tag(id) ON DELETE CASCADE,
+    post_id BIGINT REFERENCES post(id) ON DELETE CASCADE,
+    tag_id BIGINT REFERENCES tag(id) ON DELETE CASCADE,
     PRIMARY KEY (post_id, tag_id)
 );
 
 CREATE TABLE comment (
-    id SERIAL PRIMARY KEY,
-    post_id INT REFERENCES post(id) ON DELETE CASCADE,
+    id BIGSERIAL PRIMARY KEY,
+    post_id BIGINT REFERENCES post(id) ON DELETE CASCADE,
     text TEXT NOT NULL
 );
